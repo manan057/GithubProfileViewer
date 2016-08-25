499eda6c12c71635c7808464441d4068910d0f25
Manan <manan_rana@hotmail.com>
2016-08-26 07:20:10 +1000

    boilerplate + webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: [
+    // js file we create that will be compiled
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'app/js/main.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
